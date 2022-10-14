a0dda9c613f7fbc343b0fa7236c108178e2bd97f
aye aye <aanaing2397@gmail.com>
2022-10-14 11:53:18 +0630

    Media gallery

---
+++ app.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const app=express();
+app.use(express.json()); // to get body data
+require("dotenv").config();//to use port number
+const mongoose=require('mongoose');// to connect db
+mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`);// to connect with db
+const fileUpload = require('express-fileupload'); ///to upload file
+app.use(fileUpload());// to upload images
+const {saveFiles}=require("./utils/gallery");
+const path=require('path'); // to image on browser
+
+app.post("/gallery", saveFiles, (req,res,next)=>{
+res.status(200).json({msg:"file Upload", filename:req.body.images});
+}) 
+
+app.use('/upload',express.static(path.join(__dirname,'upload')));
+
+const userRoute=require("./route/userRoute");
+app.use('/user',userRoute);
+
+const postRouter=require("./route/postRoute");
+const { response } = require("express");
+const { saveFile } = require("./utils/gallery");
+app.use('/post',postRouter);
+
+app.listen(process.env.PORT,console.log(`sever is running at port ${process.env.PORT}`));
---
+++ utils/gallery.js
@@ -0,0 +1,25 @@
+const saveFile=async(req,res,next)=>{
+    let file=req.files.photo;
+    let imageName=new Date().valueOf()+"_"+file.name;
+    file.mv(`./upload/${imageName}`);
+    //req.body{"photo"}=imageName;
+    req.photo=imageName;
+    next();
+}
+
+const saveFiles=async(req, res, next)=> {
+    let imageFile=[];
+    let files=req.files.files;
+    files.forEach((file) => {
+        let filename=new Date().valueOf()+ "_" + file.name;
+        file.mv(`./upload/${filename}`);
+        imageFile.push(filename);
+    });
+    req.body["images"] = imageFile.join(",");
+    next();
+}
+
+module. exports={
+    saveFile,
+    saveFiles
+}
